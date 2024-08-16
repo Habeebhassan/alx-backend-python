15520e938480cd26e1ab5ee3ad23311dab066ac0
habeebhassan <bidehassan@gmail.com>
2024-08-15 19:59:36 -0700

    requirement fail fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Complex types - string and int/float to tuple function that takes str k and an int OR float v
+Complex types - string and int/float to tuple function 
+that takes str k and an int OR float v
 Returns a tuple
 """
 from typing import Union, Tuple
-
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
